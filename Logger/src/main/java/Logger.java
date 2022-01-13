493aa56c03ea6e845d7361b9d4f88592d5067962
Ignotas Burba <32689847+ignasftw@users.noreply.github.com>
2022-01-13 11:16:38 +0200

    Docker does not like libraries

--- Logger/src/main/java/Logger.java
+++ Logger/src/main/java/Logger.java
@@ -11,27 +11,26 @@ import java.util.List;
 public class Logger {
     public Logger(){
         int i = 0;
-        System.out.println("Information: ");
         while(i++ < 10){
-        String time = "";
-        String temp = "";
-
-        Components components = JSensors.get.components();
-        List<Cpu> cpus = components.cpus;
-        if (cpus != null) {
-            for (final Cpu cpu : cpus) {
-                if (cpu.sensors != null) {
-                        time += LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-                        List<Temperature> temps = cpu.sensors.temperatures;
-                        //temp += temps.get(temps.size() -1).name + ": ";
-                        temp += temps.get(temps.size() -1).value.toString() + " C";
-                    }
-                }
-            }
-            JSONObject json = new JSONObject();
-            json.put("time",time);
-            json.put("temp",temp);
-            System.out.println(json);
+        System.out.println("Information: " + i);
+        //String time = "";
+        //String temp = "";
+        //Components components = JSensors.get.components();
+        //List<Cpu> cpus = components.cpus;
+        //if (cpus != null) {
+        //    for (final Cpu cpu : cpus) {
+        //        if (cpu.sensors != null) {
+        //                time += LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        //                List<Temperature> temps = cpu.sensors.temperatures;
+        //                //temp += temps.get(temps.size() -1).name + ": ";
+        //                temp += temps.get(temps.size() -1).value.toString() + " C";
+        //            }
+        //        }
+        //    }
+        //    JSONObject json = new JSONObject();
+        //    json.put("time",time);
+        //    json.put("temp",temp);
+        //    System.out.println(json);
         }
     }
 }
