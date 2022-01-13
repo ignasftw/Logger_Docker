265618723974bc8671482b8530838c7abc8fea98
Ignotas Burba <32689847+ignasftw@users.noreply.github.com>
2022-01-13 10:15:37 +0200

    Running the software

---
+++ Logger/src/main/java/Logger.java
@@ -0,0 +1,37 @@
+import com.profesorfalken.jsensors.JSensors;
+import com.profesorfalken.jsensors.model.components.Components;
+import com.profesorfalken.jsensors.model.components.Cpu;
+import com.profesorfalken.jsensors.model.sensors.Temperature;
+import org.json.JSONObject;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class Logger {
+    public Logger(){
+        int i = 0;
+        System.out.println("Information: ");
+        while(i++ < 10){
+        String time = "";
+        String temp = "";
+
+        Components components = JSensors.get.components();
+        List<Cpu> cpus = components.cpus;
+        if (cpus != null) {
+            for (final Cpu cpu : cpus) {
+                if (cpu.sensors != null) {
+                        time += LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+                        List<Temperature> temps = cpu.sensors.temperatures;
+                        //temp += temps.get(temps.size() -1).name + ": ";
+                        temp += temps.get(temps.size() -1).value.toString() + " C";
+                    }
+                }
+            }
+            JSONObject json = new JSONObject();
+            json.put("time",time);
+            json.put("temp",temp);
+            System.out.println(json);
+        }
+    }
+}
